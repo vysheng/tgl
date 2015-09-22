ab8f3c533dc2883aa53b517a3102d8b9abd526b0
Ben Wiederhake <BenWiederhake.GitHub@gmx>
2015-09-22 12:38:45 +0200

    Generated crypto-switch.
    See the following tag for the generating script:
    historic/gen-switch-b0c324579b30ec01b5f4265239ab60b5b07124c5

---
+++ crypto/crypto-config.h
@@ -0,0 +1,36 @@
+/* 
+    This file is part of tgl-library
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Copyright Ben Wiederhake 2015
+*/
+
+#ifndef __TGL_CRYPTO_CRYPTO_CONFIG_H__
+#define __TGL_CRYPTO_CRYPTO_CONFIG_H__
+
+#ifdef TGL_AVOID_OPENSSL
+#define TGL_AVOID_OPENSSL_AES
+#define TGL_AVOID_OPENSSL_BN
+#define TGL_AVOID_OPENSSL_ERR
+#define TGL_AVOID_OPENSSL_HMAC
+#define TGL_AVOID_OPENSSL_MD5
+#define TGL_AVOID_OPENSSL_PEM
+#define TGL_AVOID_OPENSSL_RAND
+#define TGL_AVOID_OPENSSL_RSA
+#define TGL_AVOID_OPENSSL_SHA
+#endif
+
+#endif
---
+++ crypto/rsa_altern.c
@@ -0,0 +1,33 @@
+/* 
+    This file is part of tgl-library
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Copyright Ben Wiederhake 2015
+*/
+
+#include "crypto-config.h"
+
+#ifdef TGL_AVOID_OPENSSL_RSA
+
+// #include <gcrypt/rsa.h>
+// Or similar
+
+#include "rsa.h"
+
+/* FIXME */
+#error Not yet implemented: OpenSSL-independent defines for rsa
+
+#endif
