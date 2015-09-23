509919eb449ade7dcd8f7968f336d0398f408cc7
Ben Wiederhake <BenWiederhake.GitHub@gmx>
2015-09-23 12:13:56 +0200

    Import bn.h estimate from majn/replace-openssl.

--- crypto/bn.h
+++ crypto/bn.h
@@ -21,6 +21,24 @@
 #ifndef __TGL_CRYPTO_BN_H__
 #define __TGL_CRYPTO_BN_H__
 
-/* Declarations go here. */
+struct TGLC_bn_ctx;
+struct TGLC_bn;
+
+TGLC_bn_ctx *TGLC_bn_ctx_new (void);
+void *TGLC_bn_ctx_free (TGLC_bn_ctx* ctx);
+TGLC_bn *TGLC_bn_new (void);
+void TGLC_bn_free (TGLC_bn *a);
+void TGLC_bn_clear_free (TGLC_bn *a);
+int TGLC_bn_cmp (const TGLC_bn *a, const TGLC_bn *b);
+int TGLC_bn_is_prime (const TGLC_bn *a, int checks, void (*callback) (int, int, void *), TGLC_bn_ctx *ctx, void *cb_arg);
+int TGLC_bn_bn2bin (const TGLC_bn *a, unsigned char *to);
+TGLC_bn * TGLC_bn_bin2bn(const unsigned char *s, int len, TGLC_bn *ret);
+int TGLC_bn_set_word (TGLC_bn *a, unsigned long w);
+unsigned long TGLC_bn_get_word (const TGLC_bn *a);
+int TGLC_bn_num_bytes (const TGLC_bn *a);
+int TGLC_bn_num_bits (const TGLC_bn *a);
+int TGLC_bn_sub (TGLC_bn *r, const TGLC_bn *a, const TGLC_bn *b);
+int TGLC_bn_div (TGLC_bn *dv, TGLC_bn *rem, const TGLC_bn *a, const TGLC_bn *d, TGLC_bn_ctx *ctx);
+int TGLC_bn_mod_exp (TGLC_bn *r, const TGLC_bn *a, const TGLC_bn *p, const TGLC_bn *m, TGLC_bn_ctx *ctx);
 
 #endif
