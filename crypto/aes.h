f42e116f0c8c5919c110ef3c20ce7353dafe1f0b
Ben Wiederhake <BenWiederhake.GitHub@gmx>
2015-09-25 15:51:17 +0200

    Simplify AES interface.

--- crypto/aes.h
+++ crypto/aes.h
@@ -35,8 +35,8 @@ typedef struct TGLC_aes_key {
               ];
 } TGLC_aes_key;
 
-int TGLC_aes_set_encrypt_key (const unsigned char *userKey, const int bits, TGLC_aes_key *key);
-int TGLC_aes_set_decrypt_key (const unsigned char *userKey, const int bits, TGLC_aes_key *key);
+void TGLC_aes_set_encrypt_key (const unsigned char *userKey, const int bits, TGLC_aes_key *key);
+void TGLC_aes_set_decrypt_key (const unsigned char *userKey, const int bits, TGLC_aes_key *key);
 void TGLC_aes_ige_encrypt (const unsigned char *in, unsigned char *out, size_t length, const TGLC_aes_key *key, unsigned char *ivec, const int enc);
 
 #endif
